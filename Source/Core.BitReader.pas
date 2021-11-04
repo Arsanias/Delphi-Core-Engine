a785c66ca4273dc531b63d35da29de8becb7e0a1
Arsanias <murat.karaguezel@hotmail.com>
2021-11-04 01:05:15 +0100

    Update Core.BitReader.pas

--- Source/Core.BitReader.pas
+++ Source/Core.BitReader.pas
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 Arsanias
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
 unit
   Core.BitReader;
 
